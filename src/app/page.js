fe3d2fde6efa67998dd506940924438d87ab9139
joao pedro <joaopedropalini51@gmail.com>
2023-12-15 18:35:41 -0300

    add: arquivos iniciais

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+"use client";
+import ButtonAppBar from '@/components/bar'
+import SignIn from '@/components/login';
+
+
+export default function Home() {
+  return (
+    <>
+    <ButtonAppBar/>
+    <SignIn />
+    </>
+   
+ )
+}
